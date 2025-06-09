cc44be0f438e87eeb7676f9488e56496f32f47cd
Nick Blackhall <kvuenick@gmail.com>
2025-06-09 04:01:55 +0000

    that didn't work, for the area OUTSIDE the border, but interestingly, it

--- src/components/ui/CustomCardFrame.tsx
+++ src/components/ui/CustomCardFrame.tsx
@@ -15,30 +15,36 @@ const CustomCardFrame: React.FC<CustomCardFrameProps> = ({ texturePath, classNam
       viewBox="0 0 100 100"
       preserveAspectRatio="none"
       xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
+      xmlnsXlink="http://www.w3.org/1999/xlink" // Ensure xlink namespace for href in image
     >
       <defs>
-        <filter id="roughenFilterUniqueCardFrame">
+        <filter id="roughenFilter">
           <feTurbulence type="fractalNoise" baseFrequency="0.06" numOctaves="1" result="noise" />
           <feDisplacementMap in="SourceGraphic" in2="noise" scale="0.6" />
         </filter>
+        
+        <pattern id="redTexturePattern" patternUnits="userSpaceOnUse" width="100" height="100">
+          {/* The image inside the pattern will be stretched to fill the pattern's 100x100 area */}
+          <image href={texturePath} xlinkHref={texturePath} x="0" y="0" width="100" height="100" preserveAspectRatio="none"/>
+        </pattern>
+
         <clipPath id="cardClipPath">
-          {/* Adjusted dimensions to be inset from the outer border's path */}
+          {/* This clipPath is inset from the outer border's path */}
           <rect x="3.05" y="3.05" width="93.9" height="93.9" rx="6.95" ry="6.95" />
         </clipPath>
       </defs>
-      {/* Background texture image, clipped */}
-      <image
-        href={texturePath}
-        xlinkHref={texturePath} // Fallback for older SVG viewers
+
+      {/* Background: A rectangle filled with the texture pattern, and clipped */}
+      <rect
         x="0"
         y="0"
         width="100"
         height="100"
-        preserveAspectRatio="none"
+        fill="url(#redTexturePattern)"
         clipPath="url(#cardClipPath)"
       />
-      {/* Outer border */}
+
+      {/* Outer border, drawn on top of the clipped background */}
       <rect
         x="2"
         y="2"
@@ -49,9 +55,9 @@ const CustomCardFrame: React.FC<CustomCardFrameProps> = ({ texturePath, classNam
         stroke="#000000"
         strokeWidth="2.1"
         fill="none"
-        filter="url(#roughenFilterUniqueCardFrame)"
+        filter="url(#roughenFilter)"
       />
-      {/* Inner border */}
+      {/* Inner border, also on top */}
       <rect
         x="6"
         y="6"
@@ -62,7 +68,7 @@ const CustomCardFrame: React.FC<CustomCardFrameProps> = ({ texturePath, classNam
         stroke="#000000"
         strokeWidth="0.7"
         fill="none"
-        filter="url(#roughenFilterUniqueCardFrame)"
+        filter="url(#roughenFilter)"
       />
     </svg>
   );
