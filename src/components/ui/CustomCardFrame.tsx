13f8a770bb21177dc389aec06d90d3e5825bb075
Nick Blackhall <kvuenick@gmail.com>
2025-06-09 03:53:04 +0000

    oof. ok that doesn't look great. how can we make that look better? the b

--- src/components/ui/CustomCardFrame.tsx
+++ src/components/ui/CustomCardFrame.tsx
@@ -11,22 +11,34 @@ interface CustomCardFrameProps {
 const CustomCardFrame: React.FC<CustomCardFrameProps> = ({ texturePath, className }) => {
   return (
     <svg
-      className={className} // Pass className for sizing/positioning from parent
+      className={className}
       viewBox="0 0 100 100"
-      preserveAspectRatio="none" // This will stretch the image
+      preserveAspectRatio="none"
       xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink" // For href in <image>
+      xmlnsXlink="http://www.w3.org/1999/xlink"
     >
       <defs>
-        {/* Unique filter ID to avoid clashes if multiple SVGs are on the page */}
         <filter id="roughenFilterUniqueCardFrame">
-          <feTurbulence type="fractalNoise" baseFrequency="0.015" numOctaves="1" result="noise" />
-          <feDisplacementMap in="SourceGraphic" in2="noise" scale="1.2" />
+          {/* Adjusted filter parameters for a potentially more noticeable effect */}
+          <feTurbulence type="fractalNoise" baseFrequency="0.02" numOctaves="1" result="noise" />
+          <feDisplacementMap in="SourceGraphic" in2="noise" scale="1.3" />
         </filter>
+        <clipPath id="cardClipPath">
+          {/* This rect defines the clipping area, matching the outer border's shape */}
+          <rect x="2" y="2" width="96" height="96" rx="8" ry="8" />
+        </clipPath>
       </defs>
-      {/* Background texture image */}
-      <image href={texturePath} x="0" y="0" width="100" height="100" preserveAspectRatio="none" />
-      {/* Outer border */}
+      {/* Background texture image, now clipped */}
+      <image
+        href={texturePath}
+        x="0"
+        y="0"
+        width="100"
+        height="100"
+        preserveAspectRatio="none"
+        clipPath="url(#cardClipPath)"
+      />
+      {/* Outer border - reduced thickness */}
       <rect
         x="2"
         y="2"
@@ -35,11 +47,11 @@ const CustomCardFrame: React.FC<CustomCardFrameProps> = ({ texturePath, classNam
         rx="8"
         ry="8"
         stroke="#000000"
-        strokeWidth="4" // JSX uses camelCase for attributes like strokeWidth
+        strokeWidth="3" // Reduced from 4
         fill="none"
         filter="url(#roughenFilterUniqueCardFrame)"
       />
-      {/* Inner border */}
+      {/* Inner border - reduced thickness */}
       <rect
         x="6"
         y="6"
@@ -48,7 +60,7 @@ const CustomCardFrame: React.FC<CustomCardFrameProps> = ({ texturePath, classNam
         rx="6"
         ry="6"
         stroke="#000000"
-        strokeWidth="1.5" // JSX uses camelCase
+        strokeWidth="1" // Reduced from 1.5
         fill="none"
         filter="url(#roughenFilterUniqueCardFrame)"
       />
