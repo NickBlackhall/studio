6fbb66f4a8cee1b0ca3f190d971f1db3a49751c3
Nick Blackhall <kvuenick@gmail.com>
2025-07-09 22:32:25 +0000

    ok keep going some more, its almost correct.

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -667,7 +667,7 @@ export default function GamePage() {
                 priority
                 data-ai-hint="scoreboard poster"
             />
-            <div className="absolute top-[40%] bottom-[15%] left-[10%] right-[10%]">
+            <div className="absolute top-[42%] bottom-[15%] left-[10%] right-[10%]">
                 <Scoreboard
                     players={internalGameState.players}
                     currentJudgeId={internalGameState.currentJudgeId}
