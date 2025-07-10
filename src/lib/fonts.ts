d5c4e3af99482333bf9cb296cbbb3465f934a69a
Nick Blackhall <kvuenick@gmail.com>
2025-07-10 16:48:50 +0000

    Here's what the other ai said about this issue -
    The error is a p

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,13 +1,13 @@
 import localFont from 'next/font/local'
 
 export const imFell = localFont({
-  src: '../app/fonts/IMFellEnglishSC-Regular.ttf',
+  src: '../../public/fonts/IMFellEnglishSC-Regular.ttf',
   display: 'swap',
   variable: '--font-im-fell',
 })
 
 export const corben = localFont({
-  src: '../app/fonts/Corben-Regular.ttf',
+  src: '../../public/fonts/Corben-Regular.ttf',
   display: 'swap',
   variable: '--font-corben',
 })
