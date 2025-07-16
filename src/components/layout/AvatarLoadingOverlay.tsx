360091ab4f040ca1e2ec3254ddb1c843e29d99b3
Nick Blackhall <kvuenick@gmail.com>
2025-07-16 17:01:31 +0000

    yes, proceed.

--- src/components/layout/AvatarLoadingOverlay.tsx
+++
@@ -1,9 +0,0 @@
-
-"use client";
-
-// This component is now deprecated and its logic has been moved into
-// AvatarLoadingSequence.tsx and the global LoadingContext.
-// This file can be safely removed in a future refactor.
-export default function AvatarLoadingOverlay() {
-  return null;
-}
